cde6f4ad22efef0fb4faaebffd9275db163cbe26
Shane Wilton <shane.wilton@gmail.com>
2014-02-15 13:03:40 -0500

    Withdrawals + cleanup

--- web/routers/users_router.ex
+++ web/routers/users_router.ex
@@ -16,13 +16,32 @@ defmodule UsersRouter do
     user = Amnesia.transaction do
       user = binary_to_integer(user_id) |> User.read
       deposited = DogeAPI.get_address_received(System.get_env("DOGE_API_KEY"), user.wallet).body
-      user.balance(user.balance + deposited - user.deposited).write
-      user.deposited(deposited)
-      user
+      user.balance(user.balance + deposited - user.deposited).deposited(deposited).write
     end
 
     { :ok, json } = JSEX.encode user
     conn.resp 200, json
+  end
+
+  put "/:user_id/withdraw" do
+    amount = binary_to_integer(conn.params[:amount])
+    target_wallet = conn.params[:target_wallet]
+    result = Amnesia.transaction do
+      user = binary_to_integer(user_id) |> User.read
+      if user.balance >= amount and amount >= 5 do
+        DogeAPI.withdraw(System.get_env("DOGE_API_KEY"), amount, target_wallet)
+        user.balance(user.balance - amount).write
+      else
+        { :error, :insufficient_funds}
+      end
+    end
+
+    cond do
+      result == { :error, :insufficient_funds } -> conn.resp 422, "insufficient funds"
+      true ->
+        { :ok, json } = JSEX.encode result
+        conn.resp 200, json
+    end
   end
 
   post "/" do
